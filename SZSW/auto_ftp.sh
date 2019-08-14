e439b52a7d8cbf38c54068227c46fd235b124690
WuGang86 <wugang86@189.cn>
2019-08-14 16:41:42 +0800

    我的第一次提交

--- SZSW/auto_ftp.sh
+++ SZSW/auto_ftp.sh
@@ -1,1 +1,23 @@
+#!/bin/sh
 
+SRCDir=/arch_backup
+DSTDir=/IPAddress\(Folder name\)
+DayDir=`TZ=zt16 date +%Y_%m_%d`
+
+echo `date +%Y%m%d\ %H:%M:%S`" is begin!"
+echo 'mput '$DSTDir/$DayDir
+echo "\n"
+
+ftp -n <<!
+open IPAddress
+user user passwdor
+bin
+lcd $SRCDir/$DayDir
+cd $DSTDir/$DayDir
+promput
+mput *
+bye
+!
+
+echo "\n"
+echo `date +%Y%m%d\ %H:%M:%S`" is end!"
